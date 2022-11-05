72c0431e6bdd233d598286dcb19aecf3b3b19e82
Karthikeyan Nadesan <karthik_krish_84@yahoo.com>
2022-11-05 02:38:06 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    const url = "http://34.162.49.43:8080/employee";
+    fetch(url)
+    .then(response => response.json())
+    .then(json => this.setState({ posts: json }))
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="App">
+         <tbody>
+         <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Department</th>
+          <th>Salary</th>
+          <th>Date Of Joining</th>
+        </tr>
+        {posts.map((post,index) => (
+          <tr key={index}>
+            <td>{post.employeeID}</td>
+            <td>{post.name}</td>
+            <td>{post.dept}</td>
+            <td>{post.salary}</td>
+            <td>{post.joiningDate}</td>
+          </tr>
+        ))}
+         </tbody>
+         </div>
+    );
+  }
+}
+export default App;
